602a3ea2d28a3b4e0e1e517dccfc2d88fb8f385b
frasdor <frasunkiewicz.dorota@gmail.com>
2024-10-08 11:36:04 +0200

    added components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,3 +14,24 @@ export const App = () => {
     </div>
   );
 };
+
+import React from 'react';
+import Profile from './Profile';
+import user from '../data/user.json';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Social Media Profile</h1>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+};
+
+export default App;
