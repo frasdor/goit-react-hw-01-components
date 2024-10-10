aed28bcb1c603d376682c8543547cd14ab6084c6
frasdor <frasunkiewicz.dorota@gmail.com>
2024-10-10 15:21:51 +0200

    added App styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,30 +1,20 @@
 import React from 'react';
 
 import user from '../data/user.json';
-import { Profile } from './Profile/Profile';
+import { Profile } from '../Profile/Profile';
 
 import data from '../data/data.json';
-import { Statistics } from './Statistics/Statistics';
+import { Statistics } from '../Statistics/Statistics';
 
 import friends from '../data/friends.json'; 
-import { FriendList } from './FriendList/FriendList';
+import { FriendList } from '../FriendList/FriendList';
 
 import transactions from '../data/transactions.json';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import { TransactionHistory } from '../TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
-    <><div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <><div className={styles.container}>
       <Profile
         username={user.username}
         tag={user.tag}
